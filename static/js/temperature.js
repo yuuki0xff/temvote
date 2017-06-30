bb3c10b57c4436ec53e854961d2796207d97b198
yuuki <yuuki0xff@gmail.com>
2017-06-30 17:55:09 +0900

    wip

--- static/js/temperature.js
+++ static/js/temperature.js
@@ -1,14 +1,64 @@
+(function () {
+    "use strict";
+    var updateInterval = 10 * 1000;  // 10s
+    var status = null;
 
-var temperature;
+    // .templeture_text
+    // .button.hot
+    // .button.cold
 
-function init() {
-    temperature = 20;
-}
+    function getCurrentStatus(success, error) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/v1/status');
+        xhr.responseType = 'json';
+        xhr.onreadystatechange = function () {
+            if (xhr.status === 200 || xhr.status === 302) {
+                status = xhr.response;
+                success(status);
+            } else {
+                error();
+            }
+        };
+        xhr.send();
+    }
 
-function draw_temperature() {
-    temperature++;
-    var elem = document.getElementById("drawTemp");
-    elem.innerHTML = "現在<strong style=\"border-style: solid ; border-width: 2px;\">"+ temperature +"℃</font></strong>です";
-  
-}
+    function vote(status, success, error) {
+        var params = {
+            'vote': status
+        };
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/v1/status');
+        xhr.responseType = 'json';
+        xhr.onreadystatechange = function () {
+            if (xhr.status === 200 || xhr.status === 302) {
+                status = xhr.response;
+                success(status);
+            } else {
+                error();
+            }
+        };
+        xhr.send(params);
+    }
 
+    function showErrorMessage() {
+        alert('error');
+    }
+
+    function update() {
+        document.querySelector('.templeture_text').innerText = status.templature;
+        document.querySelector('.counter.hot').innerText = status.hot;
+        document.querySelector('.counter.cold').innerText = status.cold;
+    }
+
+    getCurrentStatus(update, showErrorMessage);
+    setInterval(function () {
+        getCurrentStatus(update, showErrorMessage);
+    }, updateInterval);
+
+    document.querySelector('.button.hot').addEventListener('click', function () {
+        vote('hot', update, showErrorMessage);
+    });
+    document.querySelector('.button.cold').addEventListener('click', function () {
+        vote('cold', update, showErrorMessage);
+    });
+})();
