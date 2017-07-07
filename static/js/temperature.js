2d9601bcdfe8a7d28fd34eecd827a3c4839316fd
yuuki <yuuki0xff@gmail.com>
2017-07-07 17:36:04 +0900

    教室名を表示できるようにした

--- static/js/temperature.js
+++ static/js/temperature.js
@@ -1,12 +1,29 @@
 /*
  * 温度と投票数を一定間隔で自動更新する。
  * 暑い・寒いボタンを押したときに、投票する。
+ * 教室名を表示する。
  */
 
 (function () {
     "use strict";
     var updateInterval = 10 * 1000;  // 10s
     var status = null;
+    var roomId2RoomName = {
+        'kougi201': '講義棟201',
+        'kougi202': '講義棟202',
+        'kougi203': '講義棟203',
+        'kougi204': '講義棟204',
+
+        'kougi301': '講義棟301',
+        'kougi302': '講義棟302',
+        'kougi303': '講義棟303',
+        'kougi304': '講義棟304'
+    };
+    var searchParams = {};
+    for(var pair of window.location.search.slice(1).split('&')) {
+        var kv = pair.split('=');
+        searchParams[kv[0]] = kv[1];
+    }
 
     // .templeture_text
     // .button.hot
@@ -60,6 +77,8 @@
         getCurrentStatus(update, showErrorMessage);
     }, updateInterval);
 
+    //////////////////////////////
+    // 投票ボタンのアクション
     document.querySelector('.button.hot').addEventListener('click', function () {
         vote('hot', update, showErrorMessage);
     });
@@ -68,5 +87,13 @@
     });
     for(var button of document.querySelectorAll('.button')) {
         button.onclick = function() {return false;};
+    }
+
+    //////////////////////////////
+    // 教室名を出力
+    if ('room' in searchParams) {
+        document.querySelector('.room_name_text').innerText = roomId2RoomName[searchParams['room']];
+    } else {
+        window.location = 'select_room.html';
     }
 })();
