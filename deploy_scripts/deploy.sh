e0c53dabbaa32d4262c5dcd6bbd7ab35a45816a3
yuuki <yuuki0xff@gmail.com>
2017-07-14 20:39:04 +0900

    fix permission

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-install --compare --owner=root --group=root --mode=200 ./version /etc/deploy/version
+install --compare --owner=root --group=root --mode=600 ./version /etc/deploy/version
 install --compare --owner=root --group=root --mode=700 ./manage /usr/local/sbin
