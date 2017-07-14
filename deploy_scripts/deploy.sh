f8541cb7d1f08618a37505076b0edd208e4b1a4e
yuuki <yuuki0xff@gmail.com>
2017-07-14 20:41:29 +0900

    install crontab

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -3,3 +3,4 @@ set -eu
 
 install --compare --owner=root --group=root --mode=600 ./version /etc/deploy/version
 install --compare --owner=root --group=root --mode=700 ./manage /usr/local/sbin
+install --compare --owner=root --group=root --mode=600 ./cron.d/deploy /etc/cron.d/deploy
