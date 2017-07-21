1a6c625f33c04ce1ca90a78dc61c8b3cdaab2871
yuuki <yuuki0xff@gmail.com>
2017-07-21 15:45:42 +0900

    create /etc/deploy/

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euv
 
+install -d --mode=700 /etc/deploy
 install --compare --owner=root --group=root --mode=600 ./version /etc/deploy/version
 install --compare --owner=root --group=root --mode=700 ./manage /usr/local/sbin
 install --compare --owner=root --group=root --mode=600 ./cron.d/deploy /etc/cron.d/deploy
