177433dfda0f5578c60f3c5aab93c8cc4bc4443c
yuuki <yuuki0xff@gmail.com>
2017-07-21 16:43:14 +0900

    add tw-node.service

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,3 +5,30 @@ install -d --mode=700 /etc/deploy
 install --compare --owner=root --group=root --mode=600 ./version /etc/deploy/version
 install --compare --owner=root --group=root --mode=700 ./manage /usr/local/sbin
 install --compare --owner=root --group=root --mode=600 ./cron.d/deploy /etc/cron.d/deploy
+
+# install tw-node
+install -d --mode=750 --owner=root --group=daemon /srv/tw-node
+if [ ! -d /srv/tw-node/.git ]; then
+    cd /srv/tw-node
+    git clone https://github.com/yuuki0xff/tw-node-example .
+fi
+# update tw-node
+pushd /srv/tw-node
+git fetch origin master
+UPDATED_TW_NODE=
+if [ "$(git rev-parse master)" != "$(git rev-parse origin/master)" ]; then
+    git pull origin master
+    UPDATED_TW_NODE=1
+fi
+popd
+
+if ! cmp ./services/tw-node.service /etc/systemd/system/tw-node.service; then
+    install --compare --owner=root --group=root ./services/tw-node.service /etc/systemd/system/tw-node.service
+    systemctl daemon-reload
+    UPDATED_TW_NODE=1
+fi
+
+if [ -n "$UPDATED_TW_NODE" ] || ; then
+    systemctl enable tw-node
+    systemctl restart tw-node
+fi
