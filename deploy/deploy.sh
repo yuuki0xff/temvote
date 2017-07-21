2b633ac38043b2a7564f340262dab3a42d124f91
yuuki <yuuki0xff@gmail.com>
2017-07-21 17:01:06 +0900

    restart cron

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,7 @@ install -d --mode=700 /etc/deploy
 install --compare --owner=root --group=root --mode=600 ./version /etc/deploy/version
 install --compare --owner=root --group=root --mode=700 ./manage /usr/local/sbin
 install --compare --owner=root --group=root --mode=600 ./cron.d/deploy /etc/cron.d/deploy
+systemctl restart cron
 
 # install tw-node
 install -d --mode=750 --owner=root --group=daemon /srv/tw-node
