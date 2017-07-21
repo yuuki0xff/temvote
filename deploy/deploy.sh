e37f24f5c9ae0f090420d6667987c273af4fbb3e
yuuki <yuuki0xff@gmail.com>
2017-07-21 17:31:35 +0900

    add tw-node config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,6 +10,7 @@ systemctl restart cron
 # install tw-node
 UPDATED_TW_NODE=
 install -d --mode=750 --owner=root --group=daemon /srv/tw-node
+install --compare --owner=root --group=root ./config/tw-node.example.yml /etc/deploy/tw-node.example.yml
 if [ ! -d /srv/tw-node/.git ]; then
     pushd /srv/tw-node
     git clone https://github.com/yuuki0xff/tw-node-example .
@@ -25,9 +26,16 @@ if [ "$(git rev-parse master)" != "$(git rev-parse origin/master)" ]; then
 fi
 popd
 
+# update service file
 if ! cmp ./services/tw-node.service /etc/systemd/system/tw-node.service; then
     install --compare --owner=root --group=root ./services/tw-node.service /etc/systemd/system/tw-node.service
     systemctl daemon-reload
+    UPDATED_TW_NODE=1
+fi
+
+# update config file
+if ! cmp /etc/deploy/tw-node.yml /srv/tw-node/conf/config.yml; then
+    install --compare --owner=root --group=root /etc/deploy/tw-node.yml /srv/tw-node/conf/config.yml
     UPDATED_TW_NODE=1
 fi
 
