ff6509662e19f4a8e71663b7ef2c46dcb35c31b3
yuuki <yuuki0xff@gmail.com>
2017-07-29 18:27:05 +0900

    fix nested-accordions does not work

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -3,14 +3,31 @@
  */
 
 (function () {
-    var button, panel;
+    var animeLength = 200;  // millisecond
+    var button;
     for (button of document.querySelectorAll('.accordions > button')) {
-        button.onclick = function () {
-            console.log('click', this);
-            this.classList.toggle("active");
+        // activeでないパネルを閉じる
+        var panel = button.nextElementSibling;
+        panel.style.maxHeight = button.classList.contains("active") ? "none" : 0;
 
+        button.onclick = function () {
             var panel = this.nextElementSibling;
-            panel.style.maxHeight = this.classList.contains("active") ? (panel.scrollHeight + "px") : null;
+            var self = this;
+            var isActive = function(){ return self.classList.contains("active"); };
+
+            // パネルが開いた状態(maxHeight == "none")から閉じるときに、アニメーションが再生されない問題の回避策。
+            // maxHeightを絶対値で指定した直後に、、
+            panel.style.maxHeight = isActive() ? (panel.scrollHeight + "px") : 0;
+            setTimeout(()=>{
+                this.classList.toggle("active");
+                panel.style.maxHeight = isActive() ? (panel.scrollHeight + "px") : 0;
+            }, 0);
+
+            // アニメーション終了後に、maxHeightの制限を取り除く。
+            // これにより、コンテンツの高さが動的に変化することにも対応できる。
+            setTimeout(()=>{
+                panel.style.maxHeight = isActive() ? "none" : 0;
+            }, animeLength);
         };
     }
 })();
