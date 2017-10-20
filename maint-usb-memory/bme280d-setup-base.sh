6c65de16c3e8d46c8bf83d7e6c70c0026942f297
yuuki <yuuki0xff@gmail.com>
2017-10-20 10:43:43 +0900

    set up WiFi before "apt update"
    有線LANがなくても、WiFi環境があればセットアップが行えるようにするため。
    いちいちRapberry Piを有線LANに接続するのは面倒くさい!

--- maint-usb-memory/bme280d-setup-base.sh
+++ maint-usb-memory/bme280d-setup-base.sh
@@ -5,13 +5,21 @@ SELF=$(readlink -f "$0")
 SELF_DIR=$(dirname "$SELF")
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt upgrade -y
-
 # install network settings
 install -Cd -o root -g root -m 700 /etc/wpa_supplicant
 install -C  -o root -g root -m 600 "${SELF_DIR}/config/wpa_supplicant.conf" /etc/wpa_supplicant/wlan0.conf
 install -C  -o root -g root -m 644 "${SELF_DIR}/config/interfaces-wlan.conf" /etc/network/interfaces.d/wlan0.conf
+systemctl restart wpa_supplicant.service
+systemctl restart networking.service
+
+# wait for connecting to the Internet
+until curl --connect-timeout 5 http://example.com; do
+    sleep 5
+done
+
+# upgrade packages
+sudo apt update
+sudo apt upgrade -y
 
 # shutdown
 cp -a "${SELF_DIR}/system-reset.sh" "/tmp"
