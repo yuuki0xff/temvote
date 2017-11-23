e3cf4046261b3d96c4cc1ecaa6a33b082da283a6
yuuki <yuuki0xff@gmail.com>
2017-11-23 18:13:09 +0900

    add sqlite3 driver

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/kelseyhightower/envconfig"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"io/ioutil"
 	"log"
